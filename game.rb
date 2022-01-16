d10a5c1c34ed8f70799781a447223ebf5064592b
Joe Donahue <donahue.joe.t@gmail.com>
2022-01-16 14:38:20 -0600

    Added a game class to control the main functionalities of the game.

---
+++ game.rb
@@ -0,0 +1,14 @@
+class Game
+  # This is the main game controller class. It holds all of the methods that are
+  # needed to progress the game.
+
+  # This is where we get our new deck and get the next player.
+  def initialize
+
+  end
+
+  # This is where the card game is controlled.
+  def play
+
+  end
+end
