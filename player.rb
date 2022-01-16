1c0179c18c92abb4b6ef95020a33211f946c6336
Joe Donahue <donahue.joe.t@gmail.com>
2022-01-16 14:34:48 -0600

    Added a class for player related methods.

---
+++ player.rb
@@ -0,0 +1,25 @@
+class Player
+  # This method initializes the player hand and name.
+  def initialize(player_name)
+    # Initialize the player name.
+    @name = player_name
+    
+    # Initialize the hand as an empty array.
+    @hand = []
+  end
+
+  # This method draws new cards.
+  def draw
+
+  end
+
+  # This method calculates the score of the current hand.
+  def hand_score
+
+  end
+
+  # This method shows the cards that are currently drawn.
+  def drawn_cards
+
+  end
+end
